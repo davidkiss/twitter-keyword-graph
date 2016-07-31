cefc8ed5784a64704bd612e8d9fa17ae619cf5ea
David Kiss <davidakiss@gmail.com>
2016-07-31 01:54:35 -0400

    updated to use SpringCypherRestGraphDatabase instead of deprecated SpringRestGraphDatabase

--- src/main/java/com/kaviddiss/keywords/Neo4JConfig.java
+++ src/main/java/com/kaviddiss/keywords/Neo4JConfig.java
@@ -4,7 +4,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
+import org.springframework.data.neo4j.rest.SpringCypherRestGraphDatabase;
 
 /**
  * Created by david on 2014-12-26.
@@ -15,6 +15,6 @@ public class Neo4JConfig {
     @Bean(destroyMethod = "shutdown")
     // Param injected from application.properties file:
     public GraphDatabaseService graphDatabaseService(final @Value("${neo4j.uri}") String neo4jUri) {
-        return new SpringRestGraphDatabase(neo4jUri);
+        return new SpringCypherRestGraphDatabase(neo4jUri);
     }
 }
